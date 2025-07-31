ca8e376d32b2c6ab31df490b29b7a5e267ca1fd2
JethvaniDharmik <122433563+JethvaniDharmik@users.noreply.github.com>
2025-07-31 15:01:24 +0530

    Updated PORT number

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ import userRouter from './routes/userRoute.js'
 
 //app config
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5555
 connectDB()
 connectCloudinary()
 
